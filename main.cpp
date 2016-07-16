12748ae04e8fee9976fd20a153587e5dc62cc02f
ornerymoose <dpardy87@gmail.com>
2016-07-16 17:55:18 -0400

    added snippet for positive whole number validation, havent tested Character validation function in a couple weeks

--- main.cpp
+++ main.cpp
@@ -3,12 +3,31 @@
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
 using std::numeric_limits;
 
 int validateWholeNumberInput(); //prototype
+string validateCharacterInput(); //prototype
 
 int main(){
-	validateWholeNumberInput();
+	//validateWholeNumberInput();
+	//validateDoubleInput();
+	validateCharacterInput();
+
+	//throw the following 13 lines into a function for accepting positive whole number input
+	int size;
+	cin >> size;
+	bool valid = true;
+	while (valid){
+		if(cin.fail() || cin.peek() != '\n' || size <= 0){
+			cin.clear();
+			cin.ignore(std::numeric_limits<int>::max(), '\n');
+			cout << "You must enter a positive integer, please try again:\n";
+			cin >> size;
+		} else {
+			valid = false;
+		}
+	}
 	return 0;
 }
 
@@ -30,3 +49,21 @@ int validateWholeNumberInput(){
 }
 
 
+
+string validateCharacterInput(){
+	char c;
+	string input;
+	//bool valid = true;
+	while (getline(cin, input) || cin.peek() != '\n'){
+    	// Iterate through the string one letter at a time.
+    	for (int i = 0; i < input.length(); i++) {
+        	c = input.at(i); // Get a char from string
+
+        	// if it's NOT within these bounds, then it's not a character
+        	if (! (( c >= 'a' && c <= 'z' ) || ( c >= 'A' && c <= 'Z' ) ) ) {
+            	cout << "Not a valid string" << endl;
+        	}
+     	}
+ 	}
+ 	return input;
+}
